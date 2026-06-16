6cfd5c5ae5f62e3a50629d60f62df27b5dfc74e2
SoniaJudithGalian <117454416+SoniaJudithGalian@users.noreply.github.com>
2026-06-16 16:05:20 -0300

    Update app_api_clima.py

--- app_api_clima.py
+++ app_api_clima.py
@@ -9,3 +9,13 @@ from skfuzzy import control as ctrl
 
 import plotly.express as px
 import plotly.graph_objects as go
+
+st.set_page_config(...)
+modelo = joblib.load("modelo_ml.pkl")
+función de la API
+título
+sidebar
+machine learning
+lógica difusa
+resultados
+gráficos
