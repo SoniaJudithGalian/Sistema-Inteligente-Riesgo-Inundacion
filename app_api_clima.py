303c9e6720dda6d857742aae18c28be94ea3e239
SoniaJudithGalian <117454416+SoniaJudithGalian@users.noreply.github.com>
2026-06-16 15:05:20 -0300

    Create app_api_clima.py

---
+++ app_api_clima.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+import requests
+
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+import plotly.express as px
+import plotly.graph_objects as go
