6a634f1da6782f3c463d35aca1ba7a41ecba1ec2
SoniaJudithGalian <117454416+SoniaJudithGalian@users.noreply.github.com>
2026-06-21 21:18:44 -0300

    Update app_api_clima.py

--- app_api_clima.py
+++ app_api_clima.py
@@ -10,21 +10,7 @@ from skfuzzy import control as ctrl
 import plotly.express as px
 import plotly.graph_objects as go
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-import joblib
-import requests
-
-import skfuzzy as fuzz
-from skfuzzy import control as ctrl
-
-import plotly.express as px
-import plotly.graph_objects as go
-
-import requests
-import pandas as pd
-import streamlit as st
+import pydeck as pdk
 
 
 
